4475ed8f668b55b9158d9d7fce2327277b9c6353
Robert Taraya <robert.taraya@gmail.com>
2013-12-23 09:41:28 -0600

    add jquery file, and js for automatic carousel effect

---
+++ app/assets/javascripts/carousel.js
@@ -0,0 +1,21 @@
+var counter = 0
+
+// this code works if you want to click through the carousel. must first uncomment button code in index.html
+// $("button").on("click", function(){
+//   counter++
+//   console.log(counter)
+//   $(".filmstrip").css("left", "-" + counter*500 + "px")
+//   if (counter === 3){
+//     counter = -1
+//   }
+// })
+
+// this code changes automatically every 2 seconds
+var autoCarousel = setInterval(function(){
+  counter++
+  console.log(counter)
+  $(".filmstrip").css("left", "-" + counter*970 + "px")
+  if (counter === 2){
+    counter = -1
+  }
+}, 8000)
