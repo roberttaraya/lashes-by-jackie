11cde51c80aa509470232963228f4c4c26f63d3d
Robert Taraya <roberttaraya@gmail.com>
2014-01-20 15:48:07 -0600

    add rails/heroku configuration for deployment

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module LashesByJackie
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Setting up the Rails Asset Pipeline for Heroku Deployment
+    config.assets.initialize_on_precompile = false
   end
 end
