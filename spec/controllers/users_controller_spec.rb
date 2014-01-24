f928ae613b01407e389447dd6063b6815d14bc16
Robert Taraya <roberttaraya@gmail.com>
2014-01-24 12:03:59 -0600

    add users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,34 +3,75 @@ require 'spec_helper'
 describe UsersController do
 
   describe "GET #index" do
-    it "renders the :index view"
+    it "renders the :index view" do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 
   describe "POST #create" do
     context "with valid attributes" do
-      it "saves the new user in the database"
-      it "signs in the new user"
-      it "redirects to users#show"
+      it "saves the new user in the database" do
+        expect{
+        post :create, user: attributes_for(:user)
+        }.to change(User, :count).by(1)
+      end
+
+      it "signs in the new user" do
+      end
+
+      it "redirects to users#show" do
+        post :create, user: attributes_for(:user)
+        expect(response).to redirect_to user_path(assigns[:user])
+      end
     end
 
     context "with invalid attributes" do
-      it "does not save the new contact in the database"
-      it "re-renders the :new template"
+      it "does not save the new contact in the database" do
+        expect{
+          post :create, user: attributes_for(:invalid_user)
+        }.to_not change(User, :count)
+      end
+
+      it "re-renders the :new template" do
+        post :create, user: attributes_for(:invalid_user)
+        expect(response).to render_template :new
+      end
     end
   end
 
   describe "GET #new" do
-    it "renders the :new template"
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
   end
 
   describe "GET #edit" do
-    it "assigns the requested user to @user"
-    it "renders the :edit template"
+    it "assigns the requested user to @user" do
+      user = create(:user)
+      get :edit, id: user
+      expect(assigns(:user)).to eq user
+    end
+
+    it "renders the :edit template" do
+      user = create(:user)
+      get :edit, id: user
+      expect(response).to render_template :edit
+    end
   end
 
   describe "GET #show" do
-    it "assigns the requested user to @user"
-    it "renders the :show template"
+    it "assigns the requested user to @user" do
+      user = create(:user)
+      get :show, id: user
+      expect(assigns(:user)).to eq user
+    end
+    it "renders the :show template" do
+      user = create(:user)
+      get :show, id: user
+      expect(response).to render_template :show
+    end
   end
 
   describe "PUT #update" do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,5 +8,9 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password "password"
     password_confirmation "password"
+
+    factory :invalid_user do
+      email nil
+    end
   end
 end
