67cadde13d05e17aeffc7ecd7f136b63ad4655ec
Robert Taraya <roberttaraya@gmail.com>
2014-01-23 21:33:51 -0600

    configure factory girl to use shortened syntax for build, commit, attributes_for and build_stubbed. refactor user tests using short syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,85 +2,52 @@ require 'spec_helper'
 
 describe User do
   it "has a valid factory" do
-    expect(FactoryGirl.build(:user)).to be_valid
+    expect(create(:user)).to be_valid
   end
 
   it "is valid with a first_name, last_name, cell_phone, email, password, password_confirmation" do
-    user = User.new(
-      first_name: "Jackie",
-      last_name: "Luu",
-      cell_phone: "469-888-0086",
-      email: "jackie@example.com",
-      password: "password",
-      password_confirmation: "password")
-    expect(user).to be_valid
+    expect(create(:user)).to be_valid
   end
 
   it "is invalid without a first name" do
-    user = FactoryGirl.build(:user, first_name: nil)
-    expect(user).to have(2).errors_on(:first_name)
+    expect(build(:user, first_name: nil)).to have(2).errors_on(:first_name)
   end
 
   it " is invalid without a first name with at least 2 characters" do
-    user = FactoryGirl.build(:user, first_name: "L")
-    expect(user).to have(1).errors_on(:first_name)
+    expect(build(:user, first_name: "L")).to have(1).errors_on(:first_name)
   end
 
   it "is invalid without an email address" do
-    user = FactoryGirl.build(:user, email: nil)
-    expect(user).to have(2).errors_on(:email)
+    expect(build(:user, email: nil)).to have(2).errors_on(:email)
   end
 
   it "is invalid with a duplicate email address" do
-    FactoryGirl.create(:user,
-      email: "duplicate@example.com")
-
-    user = FactoryGirl.build(:user,
-      email: "duplicate@example.com")
-
-    expect(user).to have(1).errors_on(:email)
+    create(:user, email: "duplicate@example.com")
+    expect(build(:user, email: "duplicate@example.com")).to have(1).errors_on(:email)
   end
 
   it "is invalid without a password" do
-    expect(User.new(password: nil)).to have(1).errors_on(:password_digest)
+    expect(build(:user, password: nil)).to have(1).errors_on(:password_digest)
   end
 
   it "is invalid without a password that is at least 6 characters long" do
-    expect(User.new(password: "pw")).to have(1).errors_on(:password)
+    expect(build(:user, password: "pw", password_confirmation: "pw")).to have(1).errors_on(:password)
   end
 
   it "is invalid without a password confirmation" do
-    expect(User.new(password_confirmation: nil)).to have(1).errors_on(:password_confirmation)
+    expect(build(:user, password_confirmation: nil)).to have(1).errors_on(:password_confirmation)
   end
 
   it "returns a user's full name as a string" do
-    user = FactoryGirl.build(:user,
-      first_name: "John",
-      last_name: "Doe")
-
+    user = build(:user, first_name: "John", last_name: "Doe")
     expect(user.name).to eq("John Doe")
   end
 
   describe "filter last name by letter" do
     before :each do
-      @smith = User.create(
-          first_name: "John",
-          last_name: "Smith",
-          email: "johnsmith@example.com",
-          password: "password",
-          password_confirmation: "password")
-      @jones = User.create(
-          first_name: "John",
-          last_name: "Jones",
-          email: "johnjones@example.com",
-          password: "password",
-          password_confirmation: "password")
-      @jackson = User.create(
-          first_name: "John",
-          last_name: "Jackson",
-          email: "johnjackson@example.com",
-          password: "password",
-          password_confirmation: "password")
+      @smith = create(:user, last_name: "Smith")
+      @jones = create(:user, last_name: "Jones")
+      @jackson = create(:user, last_name: "Jackson")
     end
 
     context "matching letters" do
